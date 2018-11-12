2ff8d3167ead1272a2a50cc729c45608b2c22a8e
Nouman Mehmood <noumanmehmood@noumans-air.nhb-gb.ibmmobiledemo.com>
2018-11-12 10:40:59 +0000

    initial commit

--- Un mes/ViewController.swift
+++ Un mes/ViewController.swift
@@ -7,14 +7,58 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+    var expensesNameArray:[String] = []
+    var amountArray:[String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+
+        // Save Core Data
+//        let newExpense = NSEntityDescription.insertNewObject(forEntityName: "Expenses", into: context)
+//
+//        newExpense.setValue("Gym", forKey: "name")
+//        newExpense.setValue("35", forKey: "amount")
+//
+//        do {
+//            try context.save()
+//            print("SAVED")
+//        } catch  {
+//
+//        }
+        
+        // Retrieve Core Data
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Expenses")
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(request)
+            
+            if results.count > 0 {
+                for result in results as! [NSManagedObject]{
+                    if let name = result.value(forKey: "name") as? String {
+                        expensesNameArray.append(name)
+                    }
+                    if let amount = result.value(forKey: "amount") as? String {
+                        amountArray.append(amount)
+                    }
+                    
+                    print(expensesNameArray)
+                    print(amountArray)
+                    
+                }
+            }
+        } catch {
+            
+        }
+        
     }
-
-
+    
 }
 
